f3fdffdf72bff9ac916454c41082d4dfac75962c
WeizheSyr <wxu137@syr.edu>
2023-10-01 15:45:08 -0400

    reachability.py working on check_intersection()'s return distance

--- rtss2023/reachability.py
+++ rtss2023/reachability.py
@@ -8,6 +8,7 @@ class Reachability:
         self.B = B
         self.P = P
         self.U = U
+        # D1
         self.t_lo = target_low
         self.t_up = target_up
         self.max_step = max_step
@@ -22,3 +23,149 @@ class Reachability:
         self.A_i_P = [self.A_i[val] @ self.P for val in range(max_step)]
         # l for support function
         self.l = np.eye(A.shape[0])
+
+        # E
+        self.E = self.reach_E()
+
+        # D
+        self.D2 = np.zeros(self.max_step)
+        self.D3 = np.zeros(self.max_step)
+        self.D4 = self.reach_D4()
+        self.D_lo = np.zeros(self.max_step)
+        self.D_up = np.zeros(self.max_step)
+
+        # result of checking intersection
+        self.intersection = np.zeros(self.max_step)
+        self.delta_theta = np.zeros(A.shape[0])
+
+    # zonotope E
+    def reach_E(self):
+        E = []
+        for i in range(self.max_step):
+            if i == 0:
+                E.append(self.A_i_B_U[0])
+            else:
+                E.append(E[-1] + self.A_i_B_U[i])
+        return E
+
+    # zonotope D4
+    def reach_D4(self):
+        D4 = []
+        for i in range(self.max_step):
+            if i == 0:
+                D4.append(self.P)
+            else:
+                D4.append(D4[-1] + self.A_i[i - 1] @ self.P)
+        return D4
+
+    # update D2, D3
+    def reach_D23(self, x_hat, theta):
+        for i in range(self.max_step):
+            self.D2[i] = self.A_i[i] @ x_hat
+            self.D3[i] = self.A_i[i] @ theta
+        return self.D2, self.D3
+
+    # bound of box D
+    def D_bound(self):
+        for i in range(self.max_step):
+            second = self.D2[i] + self.D3[i] + self.D4[i]
+            self.D_lo[i], self.D_up[i] = self.minkowski_dif(self.t_lo, self.t_up, second)
+        return self.D_lo, self.D_up
+
+    # minkowski_dif between box and zonotope
+    def minkowski_dif(self, first_lo, first_up, second: Zonotope):
+        lo = np.zeros(self.A.shape[0])
+        up = np.zeros(self.A.shape[0])
+        for i in range(self.A.shape[0]):
+            lo[i] = first_lo[i] - second.support(self.l[i], -1)
+            up[i] = first_up[i] - second.support(self.l[i])
+        return lo, up
+
+    # check dth step's intersection
+    # return 1/0: intersect or not, distance: closest point's distance
+    def check_intersection(self, d):
+        ord = self.E[d].g.shape[1]
+        dim = self.A.shape[0]
+        start = self.E[d].c
+        center = (self.D_lo[d] + self.D_up[d]) / 2
+
+        dir = np.zeros(ord)
+        move = np.zeros(ord)
+        usedout = 1
+        i = 0
+        iteration = 0
+
+        while i < ord:
+            # t = getT(closestPoint(new_low, new_up, start) - start, temp.g[:, i])
+            t = self.getT(center - start, self.E[d].g[:, i])
+            if -0.00001 <= t <= 0.00001:
+                t = 0
+            # t = getT(box.c - start, temp.g[:, i])
+            # print("t", t)
+            if t != 0:
+                usedout = 0
+                if t + dir[i] >= 1:
+                    move[i] = 1 - dir[i]
+                    dir[i] = 1
+                elif t + dir[i] <= -1:
+                    move[i] = -1 - dir[i]
+                    dir[i] = -1
+                elif -1 <= t + dir[i] <= 1:
+                    move[i] = t
+                    dir[i] = t + dir[i]
+                # print("dir", i, dir[i])
+                # print("move", move[i])
+                next = start + move[i] * self.E[d].g[:, i]
+                # print(next)
+                # distance = np.linalg.norm(next - closestPoint(new_low, new_up, next))
+                # print("distance", distance)
+                re = self.checkinBox(self.D_lo[d], self.D_up[d], next)
+                if re == 1:
+                    # print("intersect point", next)
+                    return 1, np.zeros(dim)
+                # f_low, f_up, signal = checkPass(start, next, box.c, boxG)
+                # if signal != -1:
+                #     # print("pass intersection", start, next)
+                #     break
+                start = next
+
+            if i == ord - 1:
+                # print("one iteration")
+                iteration += 1
+                if usedout == 0:
+                    i = -1
+                    usedout = 1
+                else:
+                    break
+            i += 1
+        return 0,
+
+    # projection of the line to the generator
+    def getT(self, a, b):
+        # b is generator
+        k = np.dot(a, b) / np.dot(b, b)
+        return k
+
+    # the closest point in the box to the current point
+    def closestPoint(self, low, up, point):
+        re = np.zeros(low.shape[0])
+        for i in range(low.shape[0]):
+            if low[i] <= point[i] <= up[i]:
+                re[i] = point[i]
+            elif point[i] < low[i]:
+                re[i] = low[i]
+            elif point[i] > up[i]:
+                re[i] = up[i]
+        return re
+
+    # check a point in the box
+    def checkinBox(self, low, up, point):
+        result = 1
+        for i in range(low.shape[0]):
+            if point[i] <= low[i] - 0.01:
+                result = 0
+                return result
+            if point[i] >= up[i] + 0.01:
+                result = 0
+                return result
+        return result
